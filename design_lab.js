297a30b742616c68c83044f778863a18fead2807
Sankalp Kulshreshtha <sankalp221@gmail.com>
2017-03-25 00:25:20 -0400

    Initial commit

---
+++ design_lab.js
@@ -0,0 +1,97 @@
+
+var canvasEl = document.getElementById('canvas');
+canvasEl.style.border="solid 1px black";
+
+var canvas = new fabric.Canvas('canvas');
+canvas.setHeight(750);
+canvas.setWidth(1000);
+var rect = new fabric.Rect({
+  left: 100,
+  top: 100,
+  fill: 'red',
+  width: 100,
+  height: 100
+});
+
+// "add" rectangle onto canvas
+rect.set('lockScalingX', true);
+rect.set('lockScalingY', true);
+
+rect.set('cornerSize', 15);
+rect.set('cornerColor', 'blue');
+rect.setControlsVisibility({
+    mt: false,
+    mb: false,
+    ml: false,
+    mr: false,
+    tr: false,
+    tl: false,
+    br: false,
+    bl: false
+});
+rect.set('hasRotatingPoint', true);
+canvas.add(rect);
+
+function renderImage(file) {
+
+  // generate a new FileReader object
+  var reader = new FileReader();
+  var filter = new fabric.Image.filters.RemoveWhite({
+    threshold: 40,
+    distance: 140
+  });
+
+  // inject an image with the src url
+  reader.onload = function(event) {
+    imgElement = document.createElement('img');
+    imgElement.src = event.target.result;
+    var imgInstance = new fabric.Image(imgElement, {
+      left: 100,
+      top: 100,
+    });
+    imgInstance.setControlsVisibility({
+        mt: false,
+        mb: false,
+        ml: false,
+        mr: false,
+        tr: false,
+        tl: false,
+        br: false,
+        bl: false
+    });
+    imgInstance.set('hasRotatingPoint', true);
+    imgInstance.filters.push(filter);
+    imgInstance.applyFilters(canvas.renderAll.bind(canvas))
+    canvas.add(imgInstance);
+  }
+  // when the file is read it triggers the onload event above.
+  reader.readAsDataURL(file);
+}
+
+function renderBackground(file){
+  // generate a new FileReader object
+  var reader = new FileReader();
+
+  // inject an image with the src url
+  reader.onload = function(event) {
+    var imgElement = document.createElement('img');
+    imgElement.src = event.target.result;
+    var imgInstance = new fabric.Image(imgElement);
+    canvas.setBackgroundImage(imgInstance, canvas.renderAll.bind(canvas), {
+      originX: 'left',
+      originY: 'top',
+      left: 0,
+      top: 0
+    });
+  }
+  reader.readAsDataURL(file);
+}
+
+document.getElementById('file-input').onchange = function() {
+    renderImage(this.files[0]);
+};
+
+
+document.getElementById('background-input').onchange = function() {
+    renderBackground(this.files[0]);
+};
